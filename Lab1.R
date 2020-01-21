84fe006499bc2ad65ca818290a0e2658f56dcc2b
kimberlykarinaj <60020288+kimberlykarinaj@users.noreply.github.com>
2020-01-21 12:28:38 -0500

    Update Lab1.R

--- Lab1.R
+++ Lab1.R
@@ -10,7 +10,7 @@ ggplot2::mpg
 
 #Other geom_ functions: contour, count, density, blank, abline, hex, map
 
-#Exercise 1a
+## Exercise 1a ##
 ggplot(data = mpg, 
        mapping = aes(x = displ, y = hwy))+
   geom_point()
@@ -22,9 +22,23 @@ ggplot(data = mpg,
 #When we plot these variables, we are being shown cars that have a specific drive (front wheel, rear wheel, 4 wheel)
 #but there is no telling the amount of cars that fit into each point as it currently stands.
 
-#Exercise 1b
+## Exercise 1b ##
 ggplot(data = mpg, 
        mapping = aes(x = displ, y = hwy, color = class))+
   geom_point()
 #In comparison, we see that compact and midsize vehicles with smaller engines have the highest hwy mileage
 #while larger vehicles with larger engines have lower hwy mileage
+
+
+## Exercise 2 ##
+bank <- read.csv("C:/Users/mcgui/Downloads/bank.csv")
+View(bank)
+
+p <- ggplot(data = bank,
+            mapping = aes(x=age,y=balance, colour = y))
+p + geom_point() +  scale_x_log10()
+p + geom_smooth() + ggtitle('Age and Existing Balance Affect Subscription Acceptances')
+
+p <- ggplot(data = bank,
+            mapping = aes(x=job, fill = y))
+p + geom_bar() + coord_flip() + ggtitle('How did different occupations affect program subscription?')
